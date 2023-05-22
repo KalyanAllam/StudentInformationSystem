f07645b6d5a5a6def73a1abf4bceb254bd9579e6
KALYAN <allam.kalyana@outlook.com>
2023-05-22 21:16:06 +0530

    Add project files.

---
+++ SIS/Controllers/EnrollmentsController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SIS.Models;
+
+namespace SIS.Controllers
+{
+    public class EnrollmentsController : Controller
+    {
+        private ContosoUniversityDataEntities2 db = new ContosoUniversityDataEntities2();
+
+        // GET: Enrollments
+        public ActionResult Index(int sid)
+        {
+            var enrollments = db.Enrollments.Include(e => e.Course).Include(e => e.Student).Include(e => e.Term).Where(a => a.StudentID == sid);
+            Session["sid"] = sid;
+            TempData["Marks"] = 100;
+            return View(enrollments.ToList());
+        }
+
+        // GET: Enrollments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Create
+        public ActionResult Create()
+        {
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title");
+            int sid = Convert.ToInt32(Session["sid"]);
+            ViewBag.StudentID = new SelectList(db.Students.Where(a => a.StudentID ==sid), "StudentID", "LastName");
+            ViewBag.TermID = new SelectList(db.Terms, "TermID", "TermName");
+            return View();
+        }
+
+        // POST: Enrollments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EnrollmentID,CourseID,StudentID,TermID,Marks,MarksObtained")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Enrollments.Add(enrollment);
+                db.SaveChanges();
+                int sid = Convert.ToInt32(Session["sid"]);
+                return RedirectToAction("Index", new {sid});
+            }
+
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title", enrollment.CourseID); 
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "LastName", enrollment.StudentID);
+            ViewBag.TermID = new SelectList(db.Terms, "TermID", "TermName", enrollment.TermID);
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title", enrollment.CourseID);
+            int sid = Convert.ToInt32(Session["sid"]);
+            ViewBag.StudentID = new SelectList(db.Students.Where(a => a.StudentID == sid), "StudentID", "LastName", enrollment.StudentID);
+            ViewBag.TermID = new SelectList(db.Terms, "TermID", "TermName", enrollment.TermID);
+            return View(enrollment);
+        }
+
+        // POST: Enrollments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EnrollmentID,CourseID,StudentID,TermID,Marks,MarksObtained")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(enrollment).State = EntityState.Modified;
+                db.SaveChanges();
+                int sid = Convert.ToInt32(Session["sid"]);
+                return RedirectToAction("Index", new { sid });
+            }
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title", enrollment.CourseID);
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "LastName", enrollment.StudentID);
+            ViewBag.TermID = new SelectList(db.Terms, "TermID", "TermName", enrollment.TermID);
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // POST: Enrollments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Enrollment enrollment = db.Enrollments.Find(id);
+            db.Enrollments.Remove(enrollment);
+            db.SaveChanges();
+            int sid = Convert.ToInt32(Session["sid"]);
+            return RedirectToAction("Index", new { sid });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SIS/Controllers/StudentsController.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SIS.Models;
+using ClosedXML;
+using ClosedXML.Excel;
+using System.IO;
+
+namespace SIS.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private ContosoUniversityDataEntities2 db = new ContosoUniversityDataEntities2();
+
+        // GET: Students
+        public ActionResult Index(string searchString)
+        {
+            var students = db.Students.Include(s => s.Class).Include(s => s.Section);
+            students = students.OrderBy(s => s.RollNo);
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(s => s.LastName.Contains(searchString)
+                                       || s.FirstName.Contains(searchString));
+            }
+
+
+            students = students.OrderBy(student => student.RollNo);
+                return View(students.ToList());
+        }
+
+
+        [HttpPost]
+        public FileResult Export()
+        {
+
+            DataTable dt = new DataTable("Student");
+            dt.Columns.AddRange(new DataColumn[7] { new DataColumn("FirstName"),
+                                            new DataColumn("LastName"),
+                                            new DataColumn("PhoneNo"),
+                                            new DataColumn("Email"),
+                                            new DataColumn("ClassName"),
+                                            new DataColumn("ClassSectionName"),
+                                              new DataColumn("RollNo")
+
+
+            });
+
+            var students = from student in db.Students
+                           select student;
+
+            foreach (var student in students)
+            {
+                dt.Rows.Add(student.FirstName, student.LastName, student.PhoneNo, student.Email, student.Class.ClassName, student.Section.ClassSectionName,student.RollNo);
+            }
+
+            using (XLWorkbook wb = new XLWorkbook())
+            {
+                wb.Worksheets.Add(dt);
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    wb.SaveAs(stream);
+                    return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Student.xlsx");
+                }
+            }
+        }
+
+
+        // GET: Students/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // GET: Students/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClassID = new SelectList(db.Classes, "ClassID", "ClassName");
+            ViewBag.SectionID = new SelectList(db.Sections, "SectionID", "ClassSectionName");
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StudentID,ClassID,LastName,FirstName,PhoneNo,Email,SectionID,RollNo")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ClassID = new SelectList(db.Classes, "ClassID", "ClassName", student.ClassID);
+            ViewBag.SectionID = new SelectList(db.Sections, "SectionID", "ClassSectionName", student.SectionID);
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClassID = new SelectList(db.Classes, "ClassID", "ClassName", student.ClassID);
+            ViewBag.SectionID = new SelectList(db.Sections, "SectionID", "ClassSectionName", student.SectionID);
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "StudentID,ClassID,LastName,FirstName,PhoneNo,Email,SectionID,RollNo")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClassID = new SelectList(db.Classes, "ClassID", "ClassName", student.ClassID);
+            ViewBag.SectionID = new SelectList(db.Sections, "SectionID", "ClassSectionName", student.SectionID);
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Student student = db.Students.Find(id);
+            db.Students.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
